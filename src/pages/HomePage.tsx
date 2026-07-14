b63c37387e0c8f703569b4b83aa9884512385b5a
Adhithya-N <adhixmag107@gmail.com>
2026-07-14 17:52:49 +0530

    first commit - setting up dashboard

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <main className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        Welcome to Restaurant Management System 🍽️
+      </h1>
+    </main>
+  );
+}
+
+export default HomePage;
